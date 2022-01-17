c2f88de9ae5da1824ee6e91719610e91152bb623
cemil <cemiw033@gmail.com>
2022-01-17 23:14:38 +0400

    events

---
+++ Events/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public delegate void Func(string t);
+
+namespace Events {
+    public class Event
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string");
+
+            string str = Console.ReadLine();
+            Events.Myclass cls = new();
+            Func funcDell = new Func(cls.Space);
+            funcDell+=cls.Reverse;
+            Run run = new Run();
+            run.runFunc(funcDell, str);
+        }
+    }
+}
---
+++ Events/myclass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Events
+{
+    public class Myclass
+    {
+
+        public void Space(string str)
+        {
+            string t="";
+            for (int i = 0; i < str.Length; i++)
+            {
+                t+=(str[i]+"_");
+            }
+            t=t.Remove(t.Length - 1, 1);
+            Console.WriteLine(t);
+        }
+        public void Reverse(string str)
+        {
+            char[] chars = str.ToCharArray();
+            Array.Reverse(chars);
+            str = new string(chars);
+            Console.WriteLine(str);
+        }
+    }
+    public class Run
+    {
+
+        public void runFunc(Func t,string t1)
+        {
+            t.Invoke(t1);
+        }
+    }
+}
